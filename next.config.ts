b2ed16eaaee7d8c0db99cd2e8b12f71ae261e321
pdroid908 <email-github-p1998nr@gmail.com>
2026-04-11 21:52:28 +0700

    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff", 
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY", 
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
