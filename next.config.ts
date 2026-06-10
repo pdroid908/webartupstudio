f74319bee99a03ebca23d4108c575c3f789a3282
pdroid908 <p1998nr@gmail.com>
2026-06-10 15:08:42 +0700

    apdet

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 const isDev = process.env.NODE_ENV !== 'production';
 const nextConfig = {
-  output: "standalone",
+  images: {
+    unoptimized: false,
+  },
   async headers() {
     return [
       {
